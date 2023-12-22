f96ec3aa13d89eb9b4f869c900ecbd619a04db6c
mkeloo <mkeloo@ufl.edu>
2023-12-21 22:08:00 -0500

    project setup

---
+++ frontend/app/page.js
@@ -0,0 +1,13 @@
+import Header from '@/components/Header';
+import HomePage from '@/components/HomePage';
+import Footer from '@/components/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  );
+}
---
+++ frontend/components/Header.jsx
@@ -0,0 +1,19 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import Wrapper from './Wrapper';
+
+const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState('translate-y-0');
+  const [lasrScrollY, setLastScrollY] = useState(0);
+
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <Wrapper>Hellow From Wrapper</Wrapper>
+    </header>
+  );
+};
+export default Header;
